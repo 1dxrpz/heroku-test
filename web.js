b72fe0422c69636b953405323217a503feed4d5b
1dxrpz <scaphoidederps@gmail.com>
2022-04-02 23:21:39 +0300

    test

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-const PORT = 80;
+const PORT = 13373;
 
 app.get('/', (request, respond) => {
-  res.end("<h1>test</h1>");
+  respond.end("<h1>test</h1>");
 });
 
 app.listen(PORT, _ => {
